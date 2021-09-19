895e9cb9613b960c11e963f30e901ca7b6693153
sychen52 <sychen52@gmail.com>
2021-09-19 10:46:17 -0700

    fix a typo

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ local install_path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
 end
-l
 
 require "paq" {
     "savq/paq-nvim";                                        -- Let Paq manage itself
