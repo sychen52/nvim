6bfbdc805e74949d7d4edcb55be246d8aac1d13b
Shiyang Chen <Shiyang.Chen@intusurg.com>
2021-09-24 17:11:05 -0700

    esc terminal only when another nvim is not inside terminal

--- init.lua
+++ init.lua
@@ -74,7 +74,24 @@ vim.opt.hidden = true                    -- Enable background buffers
 --vim.opt.wrap = false                     -- Disable line wrap
 
 
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap=true})        --escape terminal
+vim.api.nvim_command [[
+fun! s:find_proc_in_tree(rootpid, names, accum) abort
+    if a:accum > 9 || !exists('*nvim_get_proc')
+        return v:false
+    endif
+    let p = nvim_get_proc(a:rootpid)
+    if !empty(p) && index(a:names, p.name) >= 0
+        return v:true
+    endif
+    for c in nvim_get_proc_children(a:rootpid)[:9]
+        if s:find_proc_in_tree(c, a:names, 1 + a:accum)
+            return v:true
+        endif
+    endfor
+    return v:false
+endfun
+tnoremap <silent><expr> <esc> <SID>find_proc_in_tree(b:terminal_job_pid, ['nvim'], 0) ? '<esc>' : '<c-\><c-n>'
+]]
 vim.api.nvim_set_keymap('n', '<Tab>',  ':bn<CR>', {noremap=true})           --Buffer nav
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':bp<CR>', {noremap=true})          --Buffer nav
 vim.api.nvim_set_keymap('n', '<C-j>',  '<C-w><C-j>', {noremap=true})        --Switch between windows
