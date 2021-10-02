78f99f57eb58ac3b1a98b4c3e0431ee9391e4219
sychen52 <sychen52@gmail.com>
2021-10-01 21:39:00 -0700

    change airline colorscheme and wildmode

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ vim.g["airline#extensions#tabline#enabled"] = 1
 vim.g["airline#extensions#tagbar#enabled"] = 1
 vim.g["airline_skip_empty_sections"] = 1
 vim.g["airline#extensions#tabline#ignore_bufadd_pat"] = '!|defx|gundo|nerd_tree|startify|tagbar|undotree|vimfiler'
-vim.g["airline_theme"] = 'base16'
+vim.g["airline_theme"] = 'molokai'
 
 --[[netrw]]
 vim.g["netrw_liststyle"] = 3
@@ -50,30 +50,30 @@ vim.g["netrw_list_hide"] = '.*.swp$,.*.pyc'
 
 vim.g["mapleader"] = " "
 vim.opt.background = "dark"
-vim.opt.mouse = "a"                      -- Enable mouse in all mode
+vim.opt.mouse = "a"                                         -- Enable mouse in all mode
 vim.opt.path:append("**")
-vim.opt.clipboard:append("unnamedplus")  -- Use system clipboard by default. You need xclip for this to work
-vim.opt.complete:append("kspell")        -- complete based on spell is ":set spell"
+vim.opt.clipboard:append("unnamedplus")                     -- Use system clipboard by default. You need xclip for this to work
+vim.opt.complete:append("kspell")                           -- complete based on spell is ":set spell"
 
-vim.opt.expandtab = true                 -- Use spaces instead of tabs
-vim.opt.ignorecase = true                -- Ignore case
-vim.opt.joinspaces = false               -- No double spaces with join
-vim.opt.list = true                      -- Show some invisible characters
-vim.opt.number = true                    -- Show line numbers
-vim.opt.relativenumber = true            -- Relative line numbers
-vim.opt.scrolloff = 4                    -- Lines of context
-vim.opt.shiftround = true                -- Round indent
-vim.opt.shiftwidth = 4                   -- Size of an indent
-vim.opt.sidescrolloff = 8                -- Columns of context
-vim.opt.smartcase = true                 -- Do not ignore case with capitals
-vim.opt.smartindent = true               -- Insert indents automatically
-vim.opt.splitbelow = true                -- Put new windows below current
-vim.opt.splitright = true                -- Put new windows right of current
-vim.opt.tabstop = 4                      -- Number of spaces tabs count for
-vim.opt.termguicolors = true             -- True color support
-vim.opt.hidden = true                    -- Enable background buffers
---vim.opt.wildmode = {'list', 'longest'}   -- Command-line completion mode
---vim.opt.wrap = false                     -- Disable line wrap
+vim.opt.expandtab = true                                    -- Use spaces instead of tabs
+vim.opt.ignorecase = true                                   -- Ignore case
+vim.opt.joinspaces = false                                  -- No double spaces with join
+vim.opt.list = true                                         -- Show some invisible characters
+vim.opt.number = true                                       -- Show line numbers
+vim.opt.relativenumber = true                               -- Relative line numbers
+vim.opt.scrolloff = 4                                       -- Lines of context
+vim.opt.shiftround = true                                   -- Round indent
+vim.opt.shiftwidth = 4                                      -- Size of an indent
+vim.opt.sidescrolloff = 8                                   -- Columns of context
+vim.opt.smartcase = true                                    -- Do not ignore case with capitals
+vim.opt.smartindent = true                                  -- Insert indents automatically
+vim.opt.splitbelow = true                                   -- Put new windows below current
+vim.opt.splitright = true                                   -- Put new windows right of current
+vim.opt.tabstop = 4                                         -- Number of spaces tabs count for
+vim.opt.termguicolors = true                                -- True color support
+vim.opt.hidden = true                                       -- Enable background buffers
+vim.opt.wildmode = {'longest:full'}                         -- Command-line completion mode
+--vim.opt.wrap = false                                      -- Disable line wrap
 
 vim.api.nvim_set_keymap('n', '<Tab>',  ':bn<CR>', {noremap=true})           --Buffer nav
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':bp<CR>', {noremap=true})          --Buffer nav
