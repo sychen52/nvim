36255804e3e49ceda9b4e749086311a8df153d05
Shiyang Chen <Shiyang.Chen@intusurg.com>
2021-10-14 11:12:15 -0700

    no context

--- init.lua
+++ init.lua
@@ -64,7 +64,6 @@ vim.opt.joinspaces = false                                  -- No double spaces 
 vim.opt.list = true                                         -- Show some invisible characters
 vim.opt.number = true                                       -- Show line numbers
 vim.opt.relativenumber = true                               -- Relative line numbers
-vim.opt.scrolloff = 4                                       -- Lines of context
 vim.opt.shiftround = true                                   -- Round indent
 vim.opt.shiftwidth = 4                                      -- Size of an indent
 vim.opt.sidescrolloff = 8                                   -- Columns of context
